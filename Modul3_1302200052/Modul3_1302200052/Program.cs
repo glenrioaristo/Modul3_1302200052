acd842f5d00e353c3da71573f93a9f06a7ec9cc0
glenrioaristo <glenrioaristo@student.telkomuniversity.ac.id>
2022-03-12 15:51:54 +0700

    menambahkan class KodeBuah

---
+++ Modul3_1302200052/Modul3_1302200052/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Modul3_1302200052
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            KodeBuah kodebuah = new KodeBuah();
+            kodebuah.getKodeBuah();
+        }
+    }
+}
